403934b8a2950af6e91c0be78000ef4659a5e10e
mohamedshaban <sarahmama@moe.edu.kw>
2024-12-02 19:11:30 +0300

    task solved

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const app = express();
 
+const router = require("./router");
+
+app.use(router);
 app.listen(8000, () => {
   console.log("The application is running on localhost:8000");
 });
-
-const router = express.Router();
-router.get("/", (req, res) => {
-  res.json("my name is sarah");
-});
-
-module.exports = router;
